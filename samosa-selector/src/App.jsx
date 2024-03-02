a42a9a439dc3521e04d944f6a50c6ef8d37db977
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-02 10:42:05 -0500

    update samosa picture works! now

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css';
+import { useState } from 'react';
+
+
 
 const App = () => {
+const [count, setCount] = useState(0);
+const[multiplier, setMultiplier] = useState(1);
+
+const updateCount = () => setCount(count + multiplier);
 
   return (
     <div className="App">
       <h1>Samosa Selector</h1>
-      <h2>Count</h2>
-      <img className = "samosa" src = "/public/samosa.jpg" alt = "samosa" />
+      <h2>Count: {count}</h2>
+      <img className = "samosa" src = "/public/samosa.png" alt = "samosa" onClick = {updateCount} 
+/>
 
     </div>
-  )
+  );
 }
 
 export default App
