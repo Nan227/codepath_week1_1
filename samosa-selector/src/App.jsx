8bb68043ffc8b361002ff029089d1142a5523ef6
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-02 01:17:53 -0500

    Added samosa picture and page

---
+++ samosa-selector/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Samosa Selector</h1>
+      <h2>Count</h2>
+      <img className = "samosa" src = "/public/samosa.jpg" alt = "samosa" />
+
+    </div>
+  )
+}
+
+export default App
