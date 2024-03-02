967d8ed318997c76f731c4108b0faa2d93513c72
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-02 11:15:41 -0500

    adding upgrade offtion

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -14,8 +14,25 @@ const updateCount = () => setCount(count + multiplier);
       <h1>Samosa Selector</h1>
       <h2>Count: {count}</h2>
       <img className = "samosa" src = "/public/samosa.png" alt = "samosa" onClick = {updateCount} 
-/>
-
+      
+      />
+    <div className="container">
+      <div className = "upgrade">
+        <h3>Double Stuffed 👯‍♀️</h3>
+        <p>2x per click</p>
+        <button>10 samosas</button>
+      </div>
+      <div className = "upgrade">
+        <h3>Party Pack 🎉</h3>
+        <p>5x per click</p>
+        <button>100 samosas</button>
+      </div>
+      <div className = "upgrade">
+        <h3>Full Feast 👩🏽‍🍳</h3>
+        <p>10x per click</p>
+        <button>1000 samosas</button>
+      </div>
+    </div>
     </div>
   );
 }
